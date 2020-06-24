588b9ed523e668051c00cae1cdfe090485b389e0
gilad <ggochman@gmail.com>
2020-06-24 05:19:32 +0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { WhyComponent } from './why/why.component';
+import { ReunionComponent } from './reunion/reunion.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'about-component', component: AboutComponent },
+  { path: 'why-component', component: WhyComponent },
+  { path: 'reunion-component', component: ReunionComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReunionComponent } from './reunion/reunion.component';
+import { WhyComponent } from './why/why.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReunionComponent,
+    WhyComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
